3184967300e66e066c950306ee2972220a633ac4
Harsh Kumar Singh <harshkumarsingh200205@gmail.com>
2026-09-12 23:28:48 +0530

    Time: 0 ms (100%), Space: 15.8 MB (41.67%) - LeetHub

---
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode dummy(0);
+        dummy.next = head;
+
+        ListNode* prev = &dummy;
+        ListNode* curr = head;
+
+        while (curr) {
+            // Check whether curr is the start of a duplicate group
+            if (curr->next && curr->val == curr->next->val) {
+                int duplicate = curr->val;
+
+                // Skip all nodes having this value
+                while (curr && curr->val == duplicate) {
+                    curr = curr->next;
+                }
+
+                prev->next = curr;
+            } 
+            else {
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+
+        return dummy.next;
+    }
+};
